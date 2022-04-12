7db5bd919526bab2ef86193f3fc9deefb808ccc6
jmallen1027 <66144917+jmallen1027@users.noreply.github.com>
2022-04-12 11:45:45 -0400

    Update start.sql

--- user/mysql/scripts/start.sql
+++ user/mysql/scripts/start.sql
@@ -1,4 +1,4 @@
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Vikings123!';
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY $ENTER_PW_HERE';
 FLUSH PRIVILEGES;
 SELECT user,authentication_string,plugin,host FROM mysql.user;
 CREATE DATABASE user;
