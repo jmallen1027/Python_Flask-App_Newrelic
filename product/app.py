1ee09840eca3ac98ca15edfae5ac69e28839dc8c
jmallen1027 <66144917+jmallen1027@users.noreply.github.com>
2022-04-12 11:42:33 -0400

    Update app.py

--- product/app.py
+++ product/app.py
@@ -14,8 +14,8 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'AWfi7qHc8pNYWmrwYt99CQ'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Vikings123!@db/book'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Vikings123!@localhost:3306/book'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:$ENTER_YOUR_PW_HERE@db/book'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:$ENTER_YOUR_PW_HERE@localhost:3306/book'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database/book.db'
 app.register_blueprint(book_blueprint)
 init_app(app)
