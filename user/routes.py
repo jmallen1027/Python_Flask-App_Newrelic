9424e51aa506c09bd204b8f72dd243280a35aeef
jmallen1027 <66144917+jmallen1027@users.noreply.github.com>
2022-04-14 19:52:22 -0400

    Update routes.py

--- user/routes.py
+++ user/routes.py
@@ -3,11 +3,8 @@ from models import db, User
 from flask_login import login_user, current_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
-
-
 user_blueprint = Blueprint('user_api_routes', __name__,
                            url_prefix='/api/user')
-
 
 @user_blueprint.route('/all', methods=['GET'])
 def get_all_users():
@@ -17,18 +14,14 @@ def get_all_users():
         'message': 'Returning all users',
         'result': result
     }
-
     return jsonify(response)
 
-
-@user_blueprint.route('/create', methods=['POST'])
+  @user_blueprint.route('/create', methods=['POST'])
 def create_user():
     try:
         user = User()
         user.username = request.form["username"]
-        user.password = generate_password_hash(request.form['password'],
-                                                method='sha256')
-
+        user.password = generate_password_hash(request.form['password'], method='sha256')
         user.is_admin = False
 
         db.session.add(user)
@@ -65,8 +58,7 @@ def logout():
         logout_user()
         return jsonify({'message': 'logged out'})
     return jsonify({'message': 'No user logged in'}), 401
-
-
+  
 @user_blueprint.route('/<username>/exists', methods=['GET'])
 def user_exists(username):
     user = User.query.filter_by(username=username).first()
@@ -75,8 +67,6 @@ def user_exists(username):
         return jsonify({"result": True}), 200
         
     return jsonify({"result": False}), 404
-
-
 
 @user_blueprint.route('/', methods=['GET'])
 def get_current_user():
